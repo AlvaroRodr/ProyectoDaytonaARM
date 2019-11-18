e3b6864fab2beb42ace138aaea88b01858e782ea
Álvaro Rodríguez <alvaro.rodriguez.martinez98@gmail.com>
2019-11-18 10:33:40 +0100

    -Rama privado

--- ProyectoGitHubARM/src/proyecto/com/ProyectoARM.java
+++ ProyectoGitHubARM/src/proyecto/com/ProyectoARM.java
@@ -2,14 +2,14 @@ package proyecto.com;
 
 public class ProyectoARM {
 
-	public static String veredicto(String v1, String v2 ) {		
-		if (v1.equals(v2)) {
+	private static String veredicto(String v1, String v2 ) {//Al hacer el llamado del método se le deben pasar dos parámetros para su funcionamiento	
+		if (v1.equals(v2)) { //Se comparan los parámetros pasados, los cuales han sido agregados a las variables v1 y v2, de forma que si son iguales devuelva esa cadena
 		    return "Perfecto. Has tenido buen criterio.";
 		}
-		if (Integer.valueOf(v1) < Integer.valueOf(v2)){
+		if (Integer.valueOf(v1) < Integer.valueOf(v2)){//Se comparan los parámetros pasados, los cuales son convertidos a enteros, de forma que si v1 es menor que v2 se devolverá esa cadena
 		    return "Has mejorado, o te ha llegado el éxito inesperado.";
 		}
-		return "Te has confiado. Falta realismo.";
+		return "Te has confiado. Falta realismo."; //Si no funciona ninguno de los anteriores algoritmos lógicos, devuelve esta cadena
 	}
 	
 	public static void main(String args[]) {
